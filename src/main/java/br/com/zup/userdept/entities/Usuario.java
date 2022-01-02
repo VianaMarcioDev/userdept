a451eeca31fc0c5091d93131ed53db0df0791094
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2022-01-02 16:03:27 -0300

    Criar classe Usuario

---
+++ src/main/java/br/com/zup/userdept/entities/Usuario.java
@@ -0,0 +1,45 @@
+package br.com.zup.userdept.entities;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+    private Departamento departamento;
+
+    public Usuario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+}
